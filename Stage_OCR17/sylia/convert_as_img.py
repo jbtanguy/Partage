e52c3f28ac5add3ddd7d10191dfdfdea7c80d07b
sylia <kecili.sylia@gmail.com>
2020-04-09 00:59:41 +0200

    mes premiers codes

--- Stage_OCR17/sylia/convert_as_img.py
+++ Stage_OCR17/sylia/convert_as_img.py
@@ -1,7 +1,8 @@
 
+# conversion pdf en image
 
 from pdf2image import convert_from_path
-pages = convert_from_path('net1.pdf', dpi=600)
+pages = convert_from_path('net1.pdf', dpi=300)
 for idx, page in enumerate(pages):
       page.save('mazarinade'+str(idx)+'.jpg', 'JPEG')
 
