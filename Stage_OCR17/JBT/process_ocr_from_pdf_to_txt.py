806118246da77b4d81580f61bb8d12001aff8738
jbtanguy <jbtanguy56@gmail.com>
2020-04-30 16:47:10 +0200

    OCR avec Kraken, model de simon gabay

--- Stage_OCR17/JBT/process_ocr_from_pdf_to_txt.py
+++ Stage_OCR17/JBT/process_ocr_from_pdf_to_txt.py
@@ -32,28 +32,27 @@ if __name__ == "__main__":
     dpi = options.dpi
     """
 
-    path_to_pdf_dir = './../corpora/get_pdfs_from_gallica/corpus/mazarinades/pdf/' 
-    path_to_png_dir = './../JBT/png/'
-    path_to_txts_dir = './../JBT/txt/'
+    path_to_pdf_dir = './../corpora/get_pdfs_from_gallica/corpus/mazarinades/extract/' 
+    work_dir = './../JBT/png/'
     dpi = 300
     path_to_pdf_list = glob.glob(path_to_pdf_dir + '*pdf') 
 
-
+    # 1. Conversion en images
     for path_to_pdf in path_to_pdf_list: 
         root_name_pdf = path_to_pdf.split('/')[-1].split('.')[0] 
-        root_name_png = path_to_png_dir + root_name_pdf
+        root_name_png = work_dir + root_name_pdf
         root_name_txts = path_to_txts_dir + root_name_pdf
 
         pages = convert_from_path(pdf_path=path_to_pdf, dpi=dpi) # Conversion du PDF courant en une liste d'images
         for idx, img in enumerate(pages): # parcours de toutes les images 
-            img_path = root_name_png + '_' + str(idx) + '.png'
+            img_path = root_name_png + '_' + str(idx) + '_' + str(dpi) +'.png'
             img.save(img_path, 'PNG', dpi=(dpi, dpi)) # sauvegarde de l'image (car il s'agit d'une donnée qu'on a créé)
-            # La fonction .getcolors() nous permet de trier les pages des PDF correspondant à des numérisations de mazarinades de celles générées par Gallica, ou totalement ou presque vides
-            if img.getcolors() != None:
-                if len(img.getcolors()) > 70: # Les pages issues de numérisation compte 258 couleurs, alors que les générées n'en comptent que 68
-                    # OCR with tesseract 4
-                    for lang in ['eng', 'fra']:# Pour tous les modèles de langue qu'on souhaite utiliser
-                        txt = pt.image_to_string(img, lang=lang) # OCR processing
-                        path = '_'.join([root_name_txts, str(idx), str(dpi)+'dpi', 'tesseract', lang]) + '.txt' 
-                        write_ocr_result(file_path=path, ocr_result=txt) # Sauvegarde dans un fichier .txt du résultat d'OCR
-        pages = []
+            
+    # 2. Binarisation des images avec Kraken
+    cmd = 'kraken -I \"' + work_dir + '*' + '.png' + '\" -o _binarized.png binarize'
+    os.system(cmd)
+    
+    # 3. OCR Kraken: model Simon Gabay
+    cmd = 'kraken -I \"' + work_dir + '*_binarized.png' + '\" -o _kraken_SG_model.txt segment ocr -m ./../kraken_models/kraken_CORPUS17_SimonGabay.mlmodel'
+    os.system(cmd)
+    
